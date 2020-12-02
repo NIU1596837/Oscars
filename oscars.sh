520fd50a33a1ba45ae59000df62c5e9dea123743
NIU1598893 <73493562+NIU1598893@users.noreply.github.com>
2020-12-02 19:08:16 +0100

    Update oscars.sh

--- oscars.sh
+++ oscars.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+sortir=0
+while [ $sortir == 0 ]
+do
+  echo "---------------------------------------------------------"
+  echo "Base de Dades d'actors i actrius guanyadors/es de l'Oscar"
+  echo "---------------------------------------------------------"
+  echo "1- Històric d'Oscars."
+  echo "2- Qui va guanyar ...?"
+  echo "3- Ets un Expert?"
+  echo "4- Altes, baixes i modificacions."
+  echo "0- Sortir."
+read opcio
+
+case $opcio in
+1) 
+	echo "En desenvolupament";;
+2) 
+	echo "En desenvolupament";;
+3) 
+	echo "En desenvolupament";;
+4) 
+	echo "En desenvolupament";;
+0)
+ sortir=1;;
+*)
+  echo "Error";;
+esac
+done
